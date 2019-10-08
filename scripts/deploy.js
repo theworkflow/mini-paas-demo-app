976be772370f08ee27956aa7d0ba387aad9baa86
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2019-10-08 14:03:43 -0700

    chore: rework deploy scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const mime = require('mime');
+const path = require('path');
+const s3diff = require('s3-diff');
+const s3 = new AWS.S3({ region: 'us-east-1' });
+
+const { DEPLOY_BUCKET: bucket } = process.env;
+
+const localFolder = path.resolve(__dirname, '..', 'build');
+
+s3diff({
+  aws: {
+    signatureVersion: 'v4'
+  },
+  local: localFolder,
+  remote: {
+    bucket
+  },
+  recursive: true
+}, (err, data) => {
+
+  if (err) {
+    if (err.code === 'CredentialsError' || err.code === 'ExpiredToken') {
+      console.error('Credentials not set, ensure proper credentials for the specified region');
+      process.exit(1);
+    } else {
+      console.error('Error getting diff', err);
+    }
+  } else {
+    remove(data)
+      .then(() => update(data))
+      .then(() => add(data))
+      // .then(() => doInvalidation())
+      .then(() => {
+        console.log('Finished uploading');
+      });
+  }
+});
+
+function remove(data) {
+  return Promise.all(data.missing.map(file => new Promise(resolve => {
+    console.log('removing: ', file);
+    s3.deleteObject({
+      Bucket: bucket,
+      Key: file
+    }, () => resolve());
+  })));
+}
+
+function update(data) {
+  return Promise.all(data.changed.map(file => new Promise(resolve => {
+    console.log('updating: ', file);
+    upload(file, resolve);
+  })));
+}
+
+function add(data) {
+  return Promise.all(data.extra.map(file => new Promise(resolve => {
+    console.log('adding: ', file);
+    upload(file, resolve);
+  })));
+}
+
+function upload(file, cb) {
+  const cache = 'public, max-age=31536000';
+
+  s3.putObject({
+    Bucket: bucket,
+    Key: file,
+    Body: fs.createReadStream(path.resolve(localFolder, file)),
+    ContentType: mime.getType(file),
+    CacheControl: cache,
+    ACL: 'public-read'
+  }, () => cb());
+}
